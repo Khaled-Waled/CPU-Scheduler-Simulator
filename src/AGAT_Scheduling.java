09c28cee8ce6ed1078a5abb799e6462bb53e3402
Mohamed <mohamedelhagery@gmail.com>
2021-12-28 14:50:55 +0200

    working AGAT scheduler

--- src/AGAT_Scheduling.java
+++ src/AGAT_Scheduling.java
@@ -8,83 +8,139 @@ public class AGAT_Scheduling extends Scheduler {
     }
 
     public void execute() {
-        ArrayList<Process> tempProcesses = new ArrayList<>();
-        tempProcesses.addAll(processes);
-        int quantumBeginTime = 0;
-        currentPId = 1;
+        ArrayList<Process> allPros = new ArrayList<>();
+        allPros.addAll(processes);
 
-        while (true) {
-
-            updateAGATFactor(tempProcesses);
-
-            int ind = getProcessByPId(tempProcesses, currentPId);
-
-            //non-preemptive phase
-            if (tempProcesses.get(ind).quantum * .40 > (timer - quantumBeginTime)) {
-                tempProcesses.get(ind).burstTime--;
+        ArrayList<Process> queue = new ArrayList<>();
 
 
-                GUI.receiveEvent(new Event(tempProcesses.get(ind), timer++));
-            } else { // preemptive phase
-                tempProcesses.get(ind).burstTime--;
-                int smallest = getSmallestAGATFactor(tempProcesses);
+        updateAGATFactor(allPros);
 
-                //current process doesn't have smallest AGAT factor or its time is done
-                if (currentPId != tempProcesses.get(smallest).pid || timer - quantumBeginTime == tempProcesses.get(ind).quantum) {
-                    GUI.receiveEvent(new Event(timer += contextSwitch, 1));
 
-                    // process finished its quantum
-                    if (timer - quantumBeginTime == tempProcesses.get(ind).quantum)
-                        tempProcesses.get(ind).quantum += 2;
-                        //process didnt finish its quantum
-                    else
-                        tempProcesses.get(ind).quantum += timer - quantumBeginTime;
+        int sliceTimer = 0;
 
-                    currentPId = tempProcesses.get(smallest).pid;
-                    quantumBeginTime = timer;
+        int cntr = 0;
+        while (allPros.size() > 0) {
+//            if (timer == 11) {
+//                System.out.println("here");
+//            }
+//            cntr++;
+
+            for (Process process : processes) {
+
+                if (process.arrivalTime == timer && !queue.contains(process)) {
+                    queue.add(process);
                 }
             }
 
-            if (tempProcesses.get(ind).burstTime == 0) {
-                tempProcesses.remove(ind);
-                if (tempProcesses.size() < 1) break;
-                int smallest = getSmallestAGATFactor(tempProcesses);
-                currentPId = tempProcesses.get(smallest).pid;
+            updateAGATFactor(queue);
+            if (queue.size() == 0) continue;
+
+            if (queue.get(0).burstTime == 0) {
+                sliceTimer = 0;
+                executeContextSwitch(queue);
+                allPros.remove(getProcessByPId(allPros, queue.get(0).pid));
+                queue.remove(0);
+                continue;
             }
-            if (tempProcesses.size() < 1) break;
+
+
+            //Preemptive phase
+            if (queue.get(0).quantum * 2 / 5 > sliceTimer) {
+                executeProcess(queue);
+                sliceTimer++;
+                continue;
+            }
+
+            //Non Preemptive phase
+            if (sliceTimer == queue.get(0).quantum) {
+                queue.get(0).quantum += 2;
+                sliceTimer = 0;
+                executeContextSwitch(queue);
+                queue.add(queue.get(0));
+                queue.remove(0);
+                continue;
+            }
+            if (queue.size() > 1) {
+                int smallest = getSmallestAGATFactor(queue);
+
+                if (smallest != queue.get(0).pid) {
+                    queue.get(0).quantum += queue.get(0).quantum - sliceTimer;
+                    queue.add(queue.get(0));
+                    queue.remove(0);
+
+                    ArrayList<Process> newQueue = new ArrayList<>();
+                    newQueue.add(allPros.get(getProcessByPId(allPros, smallest)));
+                    for (int i = 0; i < queue.size(); i++) {
+                        if (queue.get(i).pid == smallest) continue;
+                        newQueue.add(queue.get(i));
+                    }
+                    queue = newQueue;
+
+                    executeContextSwitch(queue);
+                    sliceTimer = 0;
+                    continue;
+                }
+            }
+
+            executeProcess(queue);
+            sliceTimer++;
         }
+
     }
 
-    void updateAGATFactor(ArrayList<Process> processes) {
-        int v1, v2;
+
+    void executeProcess(ArrayList<Process> queue) {
+        queue.get(0).burstTime--;
+        GUI.receiveEvent(new Event(queue.get(0), timer++));
+    }
+
+    void executeContextSwitch(ArrayList<Process> queue) {
+        GUI.receiveEvent(new Event(timer += contextSwitch, 1));
+
+//        queue.add(queue.get(0));
+//        queue.remove(0);
+    }
+
+    void updateAGATFactor(ArrayList<Process> pros) {
+        double v1, v2;
         int lastArrivalTime = 0, maxRemBurstTime = 0;
-        for (Process process : processes) {
+        for (Process process : pros) {
             lastArrivalTime = Math.max(lastArrivalTime, process.arrivalTime);
             maxRemBurstTime = Math.max(maxRemBurstTime, process.burstTime);
         }
 
-        if (lastArrivalTime > 10) v1 = lastArrivalTime / 10;
-        else v1 = 1;
-        if (maxRemBurstTime > 10) v2 = maxRemBurstTime / 10;
-        else v2 = 1;
+        if (lastArrivalTime > 10) v1 = lastArrivalTime / 10.0;
+        else v1 = 1.0;
+        if (maxRemBurstTime > 10) v2 = maxRemBurstTime / 10.0;
+        else v2 = 1.0;
 
-        for (Process process : processes) {
-            process.AGATFactor = (10 - process.priority) + (process.arrivalTime + v1 - 1) / v1 +
-                    (process.burstTime + v2 - 1) / v2;
+        for (Process process : pros) {
+            //if (process.arrivalTime > timer) continue;
+            process.AGATFactor = (10 - process.priority) + (int) Math.ceil((double) process.arrivalTime / v1) +
+                    (int) Math.ceil((double) process.burstTime / v2);
         }
     }
 
-    int getSmallestAGATFactor(ArrayList<Process> processes) {
-        int smallestIdx = 0;
-        int AGATFactor = Integer.MAX_VALUE;
-        for (int i = 0; i < processes.size(); i++) {
-            if (processes.get(i).arrivalTime <= timer &&
-                    processes.get(i).AGATFactor < AGATFactor) {
-                smallestIdx = i;
-                AGATFactor = processes.get(i).AGATFactor;
+    int getSmallestAGATFactor(ArrayList<Process> pros) {
+        int smallestPID = 1;
+        double AGATFactor = Integer.MAX_VALUE;
+        for (int i = 0; i < pros.size(); i++) {
+            if (pros.get(i).arrivalTime <= timer &&
+                    pros.get(i).AGATFactor < AGATFactor) {
+                smallestPID = pros.get(i).pid;
+                AGATFactor = pros.get(i).AGATFactor;
             }
         }
-        return smallestIdx;
+        return smallestPID;
+    }
+
+    void displayAGATFactor(ArrayList<Process> pros) {
+        for (int i = 0; i < pros.size(); i++) {
+            System.out.print(pros.get(i).pid + " " + pros.get(i).AGATFactor + " ");
+
+        }
+        System.out.println();
     }
 
 }
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public abstract class Scheduler {
     public ArrayList<Process> processes;
-    public int currentPId;
+    static public int currentPId;
     public int numberOfProcesses;
     public static int timer = 0;
     protected int contextSwitch;

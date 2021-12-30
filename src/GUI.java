82b59ee2568038e412f53d121a002bd9dbfb9ad7
Khaled-Waled <khaledw.hamed001@gmail.com>
2021-12-30 10:08:47 +0200

    minor GUI fixes

--- src/GUI.java
+++ src/GUI.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 
 public class GUI
 {
-    public static int numberOfProcess=1;
-    public static int totalRunningTime=1;
+    int numberOfProcesses;
     private static ArrayList<Event> events= new ArrayList<>();
 
     public GUI()
     {
         events = new ArrayList<>();
+        firstScreen();
     }
 
     static void receiveEvent(Event event)
     {
         events.add(event);
     }
-    public static void drawEvents()
+    public static void drawEventsScreen()
     {
         //TODO //GUI IMPLEMENTATION
     }
@@ -36,6 +36,56 @@ public class GUI
                 System.out.println(event.start+": IDLE");
             }
         }
+    }
+
+    public static void firstScreen()
+    {
+        //textinput at (100, 100) //numOfProcesses
+        //this.numOfProcesses = textinput.text
+
+        inputScreen();
+    }
+    public static void inputScreen()
+    {
+        ArrayList<Process> processes = new ArrayList<>();
+        int contextSwitch=0;
+        int schedulerType=0;
+
+        //textinput at (100, 100) //scheduler type
+        //schedulerType = textinput.text
+
+        //textinput at (100, 200) //Context switch time
+        //schedulerType = textinput.text
+
+        //for (int i=0; i<numberOfProcesses; i++)
+        //  generate
+        //  textinput at (100, 300+ (50*i)) //name
+        //  textinput at (200, 300+ (50*i)) //time
+        //  textinput at (300, 300+ (50*i)) //etc..
+        //  processes.add(new process(.........));
+
+
+        Scheduler scheduler;
+        switch (schedulerType)
+        {
+            case 0:
+            {
+                scheduler= new PriorityScheduler(processes, contextSwitch);
+            }
+            case 1:
+            {
+                scheduler= new ShortestFirst(processes, contextSwitch);
+            }
+            case 2:
+            {
+                scheduler= new SRTF_Scheduling(processes, contextSwitch);
+            }
+            default:
+            {
+                scheduler= new AGAT_Scheduling(processes, contextSwitch);
+            }
+        }
+        scheduler.execute();
     }
 
 }

20f9039de0361a670ee6e53c196682f5dc12dd25
Khaled-Waled <khaledw.hamed001@gmail.com>
2021-12-30 09:22:50 +0200

    Re;Added Shortset First

--- src/ShortestFirst.java
+++ src/ShortestFirst.java
@@ -11,27 +11,69 @@ public class ShortestFirst extends Scheduler
     {
         ArrayList<Process> tempPros = new ArrayList<>();
         tempPros.addAll(processes);
+        ArrayList<Process> queue = new ArrayList<Process>();
 
-        /*
-        int idx = getSmallest(tempPros);
-        for(int i=0; i<tempPros.get(idx).burstTime; i++){
-            GUI.receiveEvent(new Event(tempPros.get()));
+        int smallest = getSmallest(queue);
 
+        while (tempPros.size()>0)
+        {
+            int s = queue.size();
+
+            for (Process process : tempPros)
+            {
+                if (timer == process.arrivalTime)
+                    queue.add(process);
+                if (s == 0)
+                    smallest = getSmallest(queue);
+            }
+
+            if (queue.size() == 0) //IDLE time
+            {
+                timer++;
+                smallest = getSmallest(queue);
+                GUI.receiveEvent(new Event(timer, 2));
+                continue;
+            }
+
+            if (queue.get(smallest).pid != currentPId)  //Context Switch
+            {
+                timer += contextSwitch;
+                GUI.receiveEvent(new Event(timer, 1));
+            }
+
+            currentPId = queue.get(smallest).pid;
+            executeProcess(queue, smallest);
+
+
+            if (queue.get(smallest).burstTime == 0)
+            {
+                tempPros.removeIf(process -> process.pid == currentPId);
+                queue.remove(smallest);
+                smallest = getSmallest(queue);
+            }
         }
-*/
+    }
 
-
-
+    void executeProcess(ArrayList<Process> pros, int smallest) {
+        if (pros.get(smallest).burstTime <= 0) return;
+        pros.get(smallest).burstTime--;
+        pros.get(smallest).priority=timer;
+        GUI.receiveEvent(new Event(pros.get(smallest), timer++));
     }
 
     private int getSmallest(ArrayList<Process> pros)
     {
         int smallestIdx=0;
+        int minTime = Integer.MAX_VALUE;
         for(int i=0; i<pros.size(); i++)
         {
             if (pros.get(i).arrivalTime<= timer &&
-                    pros.get(i).burstTime< pros.get(smallestIdx).burstTime)
+                    pros.get(i).burstTime< minTime)
+            {
                 smallestIdx = i;
+                //Starvation Fix
+                minTime = pros.get(i).burstTime + pros.get(i).priority;
+            }
         }
         return smallestIdx;
     }
